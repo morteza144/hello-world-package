092cbc6347980568a14e6d7d65658d581929211f
morteza shabani pour <16mahtabi@gmail.com>
2022-01-31 01:23:07 +0330

    my first composer package

---
+++ src/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HelloWorldPackage;
+
+class HelloWorld
+{
+    public static function sayHello()
+    {
+        echo "Mori class Hello Workd funciton sayHello say to you--Hello World";
+    }
+}
